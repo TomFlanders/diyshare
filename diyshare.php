17afcca03d6d89f7d350f0469ad6f2cd1afe38a7
Tom Flanders <tom@tomflanders.com>
2020-01-01 15:13:16 -0600

    Fixed image paths

--- diyshare.php
+++ diyshare.php
@@ -1,27 +1,33 @@
 <?php
 /*
 Plugin Name: diyshare
-Plugin URI:  http://tomflanders.com/development/
+Plugin URI:  https://www.tomflanders.com/development/
 Description: DIY Share Buttons
 Version:     1.0
 Author:      Tom Flanders
-Author URI:  http://tomflanders.com
+Author URI:  https://www.tomflanders.com
 License:     GPL3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 // Adding custom text after posts
 function add_custom_text_under_posts($content){
+  $picdir = plugins_url();
+
   $content .= '<a target="_blank" href="http://www.facebook.com/sharer.php?u=';
   $content .= get_permalink();
   $content .= "&t=";
-  $content .= get_the_title();
-  $content .= '"  title="Share on facebook"><img src="http://tomflanders.com/wp-content/plugins/diyshare/facebook.png" alt="Facebook Logo"></a>';
+  $content .= urlencode(get_the_title());  
+  $content .= '"  title="Share on facebook"><img src="';
+  $content .= $picdir;
+  $content .= '/diyshare/facebook.png" alt="Facebook Logo" width="32" height="32"></a>';
 
   $content .= '<a href="http://twitter.com/share?text=';
   $content .= urlencode(get_the_title());
   $content .= '&url=';
   $content .= get_permalink();
-  $content .= '&via=twitter&related=" target="_blank" title="Share on twitter"><img src="http://tomflanders.com/wp-content/plugins/diyshare/twitter.png" alt="Twitter Logo"></a>';
+  $content .= '&via=twitter&related=" target="_blank" title="Share on twitter"><img src="';
+  $content .= $picdir;
+  $content .= '/diyshare/twitter.png" alt="Twitter Logo" width="32" height="32"></a>';
 
   return $content;
    }
